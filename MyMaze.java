6cd76d1291b39251b48b869e63587ace7d4d01e5
Samantha Xiong <xion1884@umn.edu>
2020-12-02 12:20:10 -0600

    change some stuff

--- MyMaze.java
+++ MyMaze.java
@@ -1,6 +1,7 @@
-// Names:
-// x500s:
+// Names: Samantha Xiong, Alex Xiong
+// x500s: xion1884, xion1889
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class MyMaze{
@@ -8,71 +9,66 @@ public class MyMaze{
 
     public MyMaze(int rows, int cols) {
         maze = new Cell[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                maze[i][j] = new Cell(i,j);
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                maze[i][j] = new Cell(i, j);
             }
         }
     }
 
     /* TODO: Create a new maze using the algorithm found in the writeup. */
     public static MyMaze makeMaze(int rows, int cols) {
-        MyMaze newMaze = new MyMaze(rows, cols);
-        Stack1Gen<Cell> cellStack = new Stack1Gen<>();
-        cellStack.push(newMaze.maze[0][0]);
-        newMaze.maze[0][0].setVisited(true);
+        MyMaze maze = new MyMaze(rows, cols);
+        Stack1Gen<Cell> stack = new Stack1Gen<>();
+        stack.push(maze.maze[0][0]);
+        maze.maze[0][0].setVisited(true);
+
         Random random = new Random();
-        int count = 0;
-        while (count != 10) {
-            Cell top = cellStack.top();
-            if (random.nextInt(2) == 0 && top.getCol() < newMaze.maze[0].length - 1 && top.getRow() < newMaze.maze.length - 1) {
-                Cell neighborRight = newMaze.maze[top.getRow()][top.getCol() + 1];
-                Cell neighborBottom = newMaze.maze[top.getRow() + 1][top.getCol()];
-                if (!neighborRight.getVisited()) {
-                    cellStack.push(neighborRight);
-                    neighborRight.setVisited(true);
-                    neighborRight.setRight(false);
-                    System.out.println("+1!!!!");
-                    count++;
-                }
-                System.out.println("Right visit: " + neighborRight.getVisited());
-                System.out.println("Bottom visit: " + neighborBottom.getVisited());
-                if (neighborRight.getVisited() && neighborBottom.getVisited()) {
-                    System.out.println("POP!!!!!");
-                    cellStack.pop();
-                }
-            } else if (random.nextInt(2) == 1 && top.getRow() < newMaze.maze.length - 1 && top.getCol() < newMaze.maze[0].length - 1) {
-                Cell neighborRight = newMaze.maze[top.getRow()][top.getCol() + 1];
-                Cell neighborBottom = newMaze.maze[top.getRow() + 1][top.getCol()];
-                if (!neighborBottom.getVisited()) {
-                    cellStack.push(neighborBottom);
-                    neighborBottom.setVisited(true);
-                    neighborBottom.setBottom(false);
-                    System.out.println("+1!!!!");
-                    count++;
-                }
-                System.out.println("Right visit: " + neighborRight.getVisited());
-                System.out.println("Bottom visit: " + neighborBottom.getVisited());
-                if (neighborRight.getVisited() && neighborBottom.getVisited()) {
-                    System.out.println("POP!!!!!");
-                    cellStack.pop();
-                }
-            } else {
-                System.out.println("ELSE + 1");
-                count++;
-            }
-            System.out.println("ROW: " + top.getRow() + " COL: " + top.getCol() + "\n");
+        while (!stack.isEmpty()) {
+            Cell top = stack.top();
+            int neighborCell = random.nextInt(4);
+            if (neighborCell == 0 && top.getRow() > 0 && !maze.maze[top.getRow() - 1][top.getCol()].getVisited() && top.getRow() - 1 >= 0){
+                stack.push(maze.maze[top.getRow() - 1][top.getCol()]);
+                maze.maze[top.getRow() - 1][top.getCol()].setVisited(true);
+                maze.maze[top.getRow() - 1][top.getCol()].setBottom(false);
+            } // neighbor above
+            else if (neighborCell == 1 && top.getCol() < cols - 1 && !maze.maze[top.getRow()][top.getCol() +1].getVisited()) {
+                stack.push(maze.maze[top.getRow()][top.getCol() +1]);
+                maze.maze[top.getRow()][top.getCol() +1].setVisited(true);
+                maze.maze[top.getRow()][top.getCol() +1].setRight(false);
+            } // neighbor right
+            else if (neighborCell == 2 && top.getRow() < rows - 1 && !maze.maze[top.getRow() + 1][top.getCol()].getVisited()) {
+                stack.push(maze.maze[top.getRow() + 1][top.getCol()]);
+                maze.maze[top.getRow() + 1][top.getCol()].setVisited(true);
+                top.setBottom(false);
+            } // neighbor below
+            else if (neighborCell == 3 && top.getCol() > 0 && !maze.maze[top.getRow()][top.getCol() - 1].getVisited() && top.getCol() - 1 >= 0) {
+                stack.push(maze.maze[top.getRow()][top.getCol() - 1]);
+                maze.maze[top.getRow()][top.getCol() - 1].setVisited(true);
+                top.setRight(false);
+            } // neighbor left
+            else {
+                stack.pop();
+            } // pop cell
         }
-        for (int i = 0; i < newMaze.maze.length; i++) {
-            for (int j = 0; j < newMaze.maze[i].length; j++) {
-                newMaze.maze[i][j].setVisited(false);
-            }
-        }
-        return newMaze;
+
+//        for (int i = 0; i < maze.maze.length; i++) {
+//            for (int j = 0; j < maze.maze[i].length; j++) {
+//                maze.maze[i][j].setVisited(false);
+//            }
+//        }
+        maze.printMaze();
+        return maze;
     }
 
     /* TODO: Print a representation of the maze to the terminal */
     public void printMaze() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                System.out.print("-".repeat(3));
+            }
+            System.out.println();
+        }
     }
 
     /* TODO: Solve the maze using the algorithm found in the writeup. */
@@ -80,8 +76,6 @@ public class MyMaze{
     }
 
     public static void main(String[] args){
-        MyMaze test = new MyMaze(4,4);
-        makeMaze(4,4);
-        /* Any testing can be put in this main function */
+        makeMaze(5, 21);
     }
 }
